2945a69f38243db5406043072676c642c935a321
Quan Pham <quan.phamvt040103@hcmut.edu.vn>
2025-05-28 23:19:04 +0700

    define more delay times

--- src/common/define.h
+++ src/common/define.h
@@ -31,6 +31,25 @@
 #define WIFI_SSID "Fatcat"
 #define WIFI_PASS "lnss2136"
 
+// DEFINE DELAY TIME
+#define delay_connect_wifi 5000
+#define delay_mqtt 100
+#define delay_toggle_led 1000
+#define delay_read_adc 1000
+#define I2C_clockRate 400000
+#define delay_DHT20_read 5000
+#define delay_connect 1000
+#define led_callback 500
+
+// MQTT
+#define MQTT_PORT 1883
+#define MQTT_SERVER "app.coreiot.io"
+#define delay_mqtt 100
+#define MQTT_TELEMETRY "v1/devices/me/telemetry"
+#define MQTT_ATTRIBUTES "v1/devices/me/rpc/response"
+#define MQTT_SUBCRIBE_TOPIC "v1/devices/me/rpc/request/+"
+#define MAX_MSG_SIZE 1024U
+
 // TOKEN GATEWAY
 #define TOKEN_GATEWAY "I4pP9auSdf6KzYU0PmWV"
 #endif
