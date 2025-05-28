3bb47ccf0bceea6c0ea4e9a9a32d6dd36400a94e
Quan Pham <quan.phamvt040103@hcmut.edu.vn>
2025-05-28 23:03:13 +0700

    refactor main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,10 @@
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "globals.h"
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+   Serial.begin(UART_DEFAUT_BAUDRATE, SERIAL_8N1, UART_RXD_DEBUG_PIN, UART_TXD_DEBUG_PIN);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
